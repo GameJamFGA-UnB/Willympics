9232adfa061a0ff9d841ec9b72de9f729e519bef
lorenzo7377 <lorenzodavinci@hotmail.com>
2024-11-07 00:13:46 -0300

    menu inicial

---
+++ Assets/level1call.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class leve1call : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Playgame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
